99c948d9cef70383af3bafbeef86f32059018999
Сергей Липихин <football3@ukr.net>
2021-12-01 02:34:08 +0200

    Feature 1: GetTemperature_ZP function created.

---
+++ WeatherTools/WeatherTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace WeatherTools
+{
+    public class WeatherTools
+    {
+        public static int GetTemperature_ZP()
+        {
+            string info = new WebClient().DownloadString("http://api.weatherstack.com/current?access_key=1e93a9632d8f45265a3cd3949733a911&query=Zaporizhzhya");
+
+            string temperature_info_spoiled = info.Substring(info.IndexOf("\"temperature\":", 0) + "\"temperature\":".Length, 4);
+
+            string temperature_info_only = "";
+            for (int i = 0; i < temperature_info_spoiled.Length; i++)
+            {
+                if (Char.IsDigit(temperature_info_spoiled[i]) || temperature_info_spoiled[i] == '-')
+                    temperature_info_only += temperature_info_spoiled[i];
+            }
+
+            Int32.TryParse(temperature_info_only, out int temperature_info_digit);
+            return temperature_info_digit;
+        }
+    }
+}
