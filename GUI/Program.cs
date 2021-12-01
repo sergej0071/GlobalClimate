97ec5f0b365952c119032222d0b898b27b00c24c
Serhii Denysenko <sergejdenisenko007@gmail.com>
2021-12-01 03:14:34 +0200

    feature2: add interface for library.

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -5,8 +5,33 @@ namespace GUI
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {            
+
+            while (true)
+            {
+                Console.WriteLine("    Input command");
+                var str =  Console.ReadLine();
+                switch (str)
+                {
+                    case "help":
+                         Console.WriteLine("   Select command:");
+                         Console.WriteLine("   humidity - the command that displays the humidity of Zp City ");
+                         Console.WriteLine("   temperature - the command that displays the temperature of Zp City");
+                         Console.WriteLine("   end - end of work of program");
+                    break;
+                    case "temperature":
+                        Console.WriteLine("    temperature: " + WeatherTools.WeatherTools.GetTemperature_ZP() + "°C");
+                        break;
+                    case "humidity":
+                        Console.WriteLine("    humidity: " + WeatherTools.WeatherTools.GetHumidity_ZP() + "%");
+                        break;                   
+                }
+
+                if (str.Equals("end"))
+                    break;
+            }
+
+            Console.WriteLine("   Thanks!");
         }
     }
 }
